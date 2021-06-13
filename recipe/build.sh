3766b97c9eb264a7d00a3b94273f49c564875f66
Tobias Fischer <info@tobiasfischer.info>
2021-06-14 07:47:28 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,7 +15,8 @@ fi
 cmake .. \
       -G "Ninja" \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
       -DCMAKE_CXX_STANDARD=17 \
@@ -27,5 +28,6 @@ cmake --build . --config Release --target install ${NUM_PARALLEL}
 if [ ${target_platform} != "linux-ppc64le" ]; then
   # Remove test that fail on arm64: https://github.com/ignitionrobotics/ign-physics/issues/70
   # Remove test that fail on macOS: https://github.com/conda-forge/libignition-physics-feedstock/issues/13
-  ctest --output-on-failure -C Release -E "INTEGRATION_FrameSemantics2d|INTEGRATION_JointTypes2f|UNIT_Collisions_TEST|UNIT_EntityManagement_TEST|UNIT_JointFeatures_TEST|UNIT_LinkFeatures_TEST|UNIT_SDFFeatures_TEST|UNIT_SimulationFeatures_TEST"
+  # Remove test INTEGRATION_ExamplesBuild_TEST that fails on multiple platforms: https://github.com/conda-forge/libignition-physics-feedstock/pull/14
+  ctest --output-on-failure -C Release -E "INTEGRATION_FrameSemantics2d|INTEGRATION_JointTypes2f|UNIT_Collisions_TEST|UNIT_EntityManagement_TEST|UNIT_JointFeatures_TEST|UNIT_LinkFeatures_TEST|UNIT_SDFFeatures_TEST|UNIT_SimulationFeatures_TEST|INTEGRATION_ExamplesBuild_TEST"
 fi
