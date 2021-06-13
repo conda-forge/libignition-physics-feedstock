e74a338b7ec5cce5e7489a452995d6034c2c4405
Tobias-Fischer <t.fischer@imperial.ac.uk>
2021-06-14 09:23:13 +1000

    Enable osx build

--- recipe/build.sh
+++ recipe/build.sh
@@ -29,5 +29,5 @@ if [ ${target_platform} != "linux-ppc64le" ]; then
   # Remove test that fail on arm64: https://github.com/ignitionrobotics/ign-physics/issues/70
   # Remove test that fail on macOS: https://github.com/conda-forge/libignition-physics-feedstock/issues/13
   # Remove test INTEGRATION_ExamplesBuild_TEST that fails on multiple platforms: https://github.com/conda-forge/libignition-physics-feedstock/pull/14
-  ctest --output-on-failure -C Release -E "INTEGRATION_FrameSemantics2d|INTEGRATION_JointTypes2f|UNIT_Collisions_TEST|UNIT_EntityManagement_TEST|UNIT_JointFeatures_TEST|UNIT_LinkFeatures_TEST|UNIT_SDFFeatures_TEST|UNIT_SimulationFeatures_TEST|INTEGRATION_ExamplesBuild_TEST"
+  ctest --output-on-failure -C Release -E "INTEGRATION_FrameSemantics2d|INTEGRATION_JointTypes2f|UNIT_Collisions_TEST|UNIT_EntityManagement_TEST|UNIT_JointFeatures_TEST|UNIT_LinkFeatures_TEST|UNIT_SDFFeatures_TEST|UNIT_SimulationFeatures_TEST|INTEGRATION_ExamplesBuild_TEST|UNIT_WorldFeatures_TEST|UNIT_ShapeFeatures_TEST"
 fi
